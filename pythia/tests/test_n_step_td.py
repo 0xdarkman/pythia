749654dbea828cdc01dc845c075c4e42839b3dab
Bernhard <b.raml@gmx.at>
2018-01-29 08:37:57 +0100

    refactored tdn tests to use pytest

--- pythia/tests/test_n_step_td.py
+++ pythia/tests/test_n_step_td.py
@@ -1,6 +1,8 @@
 import unittest
 from collections import deque
 
+import pytest
+
 from pythia.reinforcement.e_greedy_policies import EpsilonGreedyPolicy
 from pythia.reinforcement.n_step_sarsa import NStepSarsa
 from pythia.reinforcement.q_table import QTable
@@ -10,6 +12,10 @@ class EnvironmentSim(object):
     def __init__(self):
         self.records = deque()
         self.current_records = deque()
+
+    def record_states(self, *state_rewards):
+        for sr in state_rewards:
+            self.record(sr[0], sr[1])
 
     def record(self, state, reward):
         self.records.append((state, reward))
@@ -45,99 +51,117 @@ class PolicyStub(object):
         return self.selected_actions.popleft()
 
 
-class NStepSarsaTests(unittest.TestCase):
-    def setUp(self):
-        self.env = EnvironmentSim()
-        self.policy_factory = self.make_epsilon_greedy_policy
+@pytest.fixture
+def environment_sim():
+    """Returns a simulated environment used by the TD algorithm"""
+    return EnvironmentSim()
 
-    def test_td_zero_one_action_one_state(self):
-        tdn = self.make_tdn([0])
-        self.record_states((0, 0), (1, 2))
 
-        tdn.run()
+@pytest.fixture
+def environment_spy():
+    """Returns a spying environment recording the actions performed in it."""
+    return EnvironmentSpy(4)
 
-        self.assertAlmostEqual(2, self.q_table[0, 0])
 
-    def make_tdn(self, action_space, gamma=1.0, alpha=1.0, steps=1):
-        self.q_table = QTable(action_space)
-        self.policy = self.policy_factory(self.q_table)
-        tdn = NStepSarsa(self.env, self.q_table, self.policy, action_space)
-        tdn.steps = steps
-        tdn.gamma = gamma
-        tdn.alpha = alpha
-        return tdn
+def make_n_step_td(env, action_space, gamma=1.0, alpha=1.0, steps=1):
+    q_table = QTable(action_space)
+    policy = EpsilonGreedyPolicy(q_table, 0)
+    tdn = NStepSarsa(env, q_table, policy, action_space)
+    tdn.steps = steps
+    tdn.gamma = gamma
+    tdn.alpha = alpha
 
-    def make_epsilon_greedy_policy(self, q_table):
-        return EpsilonGreedyPolicy(q_table, 0.0)
+    return tdn
 
-    def record_states(self, *state_rewards):
-        for sr in state_rewards:
-            self.env.record(sr[0], sr[1])
 
-    def test_multiple_states_no_back_propagation(self):
-        tdn = self.make_tdn([0], 0)
-        self.record_states((0, 0), (1, 2), (2, -1))
+def make_n_step_td_with_policy(env, policy, action_space):
+    q_table = QTable(action_space)
+    tdn = NStepSarsa(env, q_table, policy, action_space)
+    tdn.steps = 1
+    tdn.gamma = 1.0
+    tdn.alpha = 1.0
 
-        tdn.run()
+    return tdn
 
-        self.assertAlmostEqual(2, self.q_table[0, 0])
-        self.assertAlmostEqual(-1, self.q_table[1, 0])
 
-    def test_back_propagation(self):
-        tdn = self.make_tdn([0], 0.5, 0.5)
-        self.record_states((0, 0), (1, 2), (2, -1))
+def test_td_zero_one_action_one_state(environment_sim):
+    tdn = make_n_step_td(environment_sim, [0])
+    environment_sim.record_states((0, 0), (1, 2))
 
-        tdn.run()
+    tdn.run()
 
-        self.assertAlmostEqual(1, self.q_table[0, 0])
-        self.assertAlmostEqual(-0.5, self.q_table[1, 0])
+    assert tdn.q_function[0, 0] == 2
 
-        tdn.run()
 
-        self.assertAlmostEqual(1.375, self.q_table[0, 0])
-        self.assertAlmostEqual(-0.75, self.q_table[1, 0])
+def test_multiple_states_no_back_propagation(environment_sim):
+    tdn = make_n_step_td(environment_sim, [0], 0)
+    environment_sim.record_states((0, 0), (1, 2), (2, -1))
 
-    def test_actions_follow_policy(self):
-        self.env = EnvironmentSpy(4)
-        self.policy_factory = lambda q: PolicyStub(1, 1, 0, 1)
-        tdn = self.make_tdn([0, 1])
+    tdn.run()
 
-        tdn.run()
+    assert tdn.q_function[0, 0] == 2
+    assert tdn.q_function[1, 0] == -1
 
-        self.assertEqual(1, self.env.received_actions[0])
-        self.assertEqual(1, self.env.received_actions[1])
-        self.assertEqual(0, self.env.received_actions[2])
-        self.assertEqual(1, self.env.received_actions[3])
 
-    def test_correct_action_values_are_updated(self):
-        self.policy_factory = lambda q: PolicyStub(0, 1)
-        tdn = self.make_tdn([0, 1])
-        self.record_states((0, 0), (1, 2), (2, 3))
+def test_back_propagation_first_step(environment_sim):
+    tdn = make_n_step_td(environment_sim, [0], 0.5, 0.5)
+    environment_sim.record_states((0, 0), (1, 2), (2, -1))
 
-        tdn.run()
+    tdn.run()
 
-        self.assertAlmostEqual(2, self.q_table[0, 0])
-        self.assertAlmostEqual(3, self.q_table[1, 1])
+    assert tdn.q_function[0, 0] == 1
+    assert tdn.q_function[1, 0] == -0.5
 
-    def test_steps_greater_one(self):
-        tdn = self.make_tdn([0], 1.0, 1.0, 2)
-        self.record_states((0, 0), (1, 2), (2, -1), (3, 0), (4, 4))
 
-        tdn.run()
+def test_back_propagation_second_step(environment_sim):
+    tdn = make_n_step_td(environment_sim, [0], 0.5, 0.5)
+    environment_sim.record_states((0, 0), (1, 2), (2, -1))
 
-        self.assertAlmostEqual(1, self.q_table[0, 0])
-        self.assertAlmostEqual(-1, self.q_table[1, 0])
-        self.assertAlmostEqual(4, self.q_table[2, 0])
-        self.assertAlmostEqual(4, self.q_table[3, 0])
+    tdn.run()
+    tdn.run()
 
-    def test_gamma_falloff(self):
-        tdn = self.make_tdn([0], 0.5, 1.0, 3)
-        self.q_table[3, 0] = 2
-        self.record_states((0, 0), (1, 2), (2, -1), (3, 3), (4, 4))
+    assert tdn.q_function[0, 0] == 1.375
+    assert tdn.q_function[1, 0] == -0.75
 
-        tdn.run()
 
-        self.assertAlmostEqual(2.5, self.q_table[0, 0])
-        self.assertAlmostEqual(1.5, self.q_table[1, 0])
-        self.assertAlmostEqual(5, self.q_table[2, 0])
-        self.assertAlmostEqual(4, self.q_table[3, 0])
+def test_actions_follow_policy(environment_spy):
+    tdn = make_n_step_td_with_policy(environment_spy, PolicyStub(1, 1, 0, 1), [0, 1])
+
+    tdn.run()
+
+    assert environment_spy.received_actions == [1, 1, 0, 1]
+
+
+def test_correct_action_values_are_updated(environment_sim):
+    tdn = make_n_step_td_with_policy(environment_sim, PolicyStub(0, 1), [0, 1])
+    environment_sim.record_states((0, 0), (1, 2), (2, 3))
+
+    tdn.run()
+
+    assert 2 == tdn.q_function[0, 0]
+    assert 3 == tdn.q_function[1, 1]
+
+
+def test_steps_greater_one(environment_sim):
+    tdn = make_n_step_td(environment_sim, [0], 1.0, 1.0, 2)
+    environment_sim.record_states((0, 0), (1, 2), (2, -1), (3, 0), (4, 4))
+
+    tdn.run()
+
+    assert 1 == tdn.q_function[0, 0]
+    assert -1 == tdn.q_function[1, 0]
+    assert 4 == tdn.q_function[2, 0]
+    assert 4 == tdn.q_function[3, 0]
+
+
+def test_gamma_falloff(environment_sim):
+    tdn = make_n_step_td(environment_sim, [0], 0.5, 1.0, 3)
+    tdn.q_function[3, 0] = 2
+    environment_sim.record_states((0, 0), (1, 2), (2, -1), (3, 3), (4, 4))
+
+    tdn.run()
+
+    assert 2.5 == tdn.q_function[0, 0]
+    assert 1.5 == tdn.q_function[1, 0]
+    assert 5 == tdn.q_function[2, 0]
+    assert 4 == tdn.q_function[3, 0]
