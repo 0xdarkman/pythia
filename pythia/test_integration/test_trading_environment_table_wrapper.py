1c5d303a3d25199cc656da5a755ab2f1c16ef9aa
Bernhard <b.raml@gmx.at>
2018-02-12 22:01:57 +0100

    - refactored trading environment table wrapper integration test to use pytest

--- pythia/test_integration/test_trading_environment_table_wrapper.py
+++ pythia/test_integration/test_trading_environment_table_wrapper.py
@@ -1,99 +1,106 @@
-import unittest
+from pytest import fixture
 
 from pythia.environment.environment_wrappers import TradingEnvironmentTableWrapper
 
 
-class TradingEnvironmentTableWrapperTests(unittest.TestCase):
-    def setUp(self):
-        self.env = TradingEnvironmentTableWrapper(1000.0, "../test_integration/test_model_data.csv", 0)
-
-    def test_reset_state(self):
-        self.env.step(0)
-        s = self.env.reset()
-        self.assertSequenceEqual(self.make_state(3, 0, 0), s)
-
-    def test_step_state_hold(self):
-        s, _, _, _ = self.env.step(0)
-        self.assertSequenceEqual(self.make_state(5, 1, 0), s)
-
-    def test_record_buying_price(self):
-        self.env.step(1)
-        self.env.step(0)
-        s, _, _, _, = self.env.step(0)
-        self.assertSequenceEqual(self.make_state(1, 3, 3), s)
-
-    def test_sequential_buying(self):
-        self.env.step(1)
-        s, _, _, _, = self.env.step(1)
-        self.assertSequenceEqual(self.make_state(4, 2, 3), s)
-
-    def test_selling_state(self):
-        self.env.step(1)
-        s, _, _, _, = self.env.step(2)
-        self.assertSequenceEqual(self.make_state(4, 2, 0), s)
-
-    def test_selling_state_without_shares(self):
-        s, _, _, _ = self.env.step(2)
-        self.assertSequenceEqual(self.make_state(5, 1, 0), s)
-
-    def test_buying_and_holding_rewards(self):
-        _, r, _, _ = self.env.step(0)
-        self.assertAlmostEqual(0.0, r)
-        _, r, _, _ = self.env.step(1)
-        self.assertAlmostEqual(0.0, r)
-
-    def test_selling_rewards(self):
-        self.env.step(1)
-        _, r, _, _ = self.env.step(2)
-        self.assertAlmostEqual(2.0, r)
-
-    def test_reward_when_selling_without_shares(self):
-        _, r, _, _ = self.env.step(2)
-        self.assertAlmostEqual(0.0, r)
-        _, r, _, _ = self.env.step(2)
-        self.assertAlmostEqual(0.0, r)
-
-    def test_trading_episode(self):
-        s = self.env.reset()
-        self.assertSequenceEqual(self.make_state(3, 0, 0), s)
-        s, r, _, _ = self.env.step(0)
-        self.assertSequenceEqual(self.make_state(5, 1, 0), s)
-        self.assertAlmostEqual(0.0, r)
-        s, r, _, _ = self.env.step(1)
-        self.assertSequenceEqual(self.make_state(4, 2, 5), s)
-        self.assertAlmostEqual(0.0, r)
-        s, r, _, _ = self.env.step(2)
-        self.assertSequenceEqual(self.make_state(1, 3, 0), s)
-        self.assertAlmostEqual(-1.0, r)
-        s, r, _, _ = self.env.step(1)
-        self.assertSequenceEqual(self.make_state(2, 4, 1), s)
-        self.assertAlmostEqual(0.0, r)
-        s, r, _, _ = self.env.step(0)
-        self.assertSequenceEqual(self.make_state(5, 5, 1), s)
-        self.assertAlmostEqual(0.0, r)
-        s, r, _, _ = self.env.step(1)
-        self.assertSequenceEqual(self.make_state(6, 6, 1), s)
-        self.assertAlmostEqual(0.0, r)
-        s, r, d, _ = self.env.step(2)
-        self.assertSequenceEqual(self.make_state(4, 7, 0), s)
-        self.assertAlmostEqual(5.0, r)
-        self.assertTrue(d)
-
-    def test_penalize_invalid_actions(self):
-        e = TradingEnvironmentTableWrapper(1000.0, "test_model_data.csv", 10)
-        _, r, _, _ = e.step(1)
-        self.assertAlmostEqual(0.0, r)
-        _, r, _, _ = e.step(1)
-        self.assertAlmostEqual(-10.0, r)
-        _, r, _, _ = e.step(2)
-        self.assertAlmostEqual(1.0, r)
-        _, r, _, _ = e.step(2)
-        self.assertAlmostEqual(-10.0, r)
-
-    @staticmethod
-    def make_state(price, step, buying_price):
-        return [float(price), float(buying_price), float(step)]
+@fixture
+def env():
+    return TradingEnvironmentTableWrapper(1000.0, "../test_integration/test_model_data.csv", 0)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def make_state(price, step, buying_price):
+    return [float(price), float(buying_price), float(step)]
+
+
+def test_reset_state(env):
+    env.step(0)
+    s = env.reset()
+    assert s == make_state(3, 0, 0)
+
+
+def test_step_state_hold(env):
+    s, _, _, _ = env.step(0)
+    assert s == make_state(5, 1, 0)
+
+
+def test_record_buying_price(env):
+    env.step(1)
+    env.step(0)
+    s, _, _, _, = env.step(0)
+    assert s == make_state(1, 3, 3)
+
+
+def test_sequential_buying(env):
+    env.step(1)
+    s, _, _, _, = env.step(1)
+    assert s == make_state(4, 2, 3)
+
+
+def test_selling_state(env):
+    env.step(1)
+    s, _, _, _, = env.step(2)
+    assert s == make_state(4, 2, 0)
+
+
+def test_selling_state_without_shares(env):
+    s, _, _, _ = env.step(2)
+    assert s == make_state(5, 1, 0)
+
+
+def test_buying_and_holding_rewards(env):
+    _, r, _, _ = env.step(0)
+    assert r == 0.0
+    _, r, _, _ = env.step(1)
+    assert r == 0.0
+
+
+def test_selling_rewards(env):
+    env.step(1)
+    _, r, _, _ = env.step(2)
+    assert r == 2.0
+
+
+def test_reward_when_selling_without_shares(env):
+    _, r, _, _ = env.step(2)
+    assert r == 0.0
+    _, r, _, _ = env.step(2)
+    assert r == 0.0
+
+
+def test_trading_episode(env):
+    s = env.reset()
+    assert s == make_state(3, 0, 0)
+    s, r, _, _ = env.step(0)
+    assert s == make_state(5, 1, 0)
+    assert r == 0.0
+    s, r, _, _ = env.step(1)
+    assert s == make_state(4, 2, 5)
+    assert r == 0.0
+    s, r, _, _ = env.step(2)
+    assert s == make_state(1, 3, 0)
+    assert r == -1.0
+    s, r, _, _ = env.step(1)
+    assert s == make_state(2, 4, 1)
+    assert r == 0.0
+    s, r, _, _ = env.step(0)
+    assert s == make_state(5, 5, 1)
+    assert r == 0.0
+    s, r, _, _ = env.step(1)
+    assert s == make_state(6, 6, 1)
+    assert r == 0.0
+    s, r, d, _ = env.step(2)
+    assert s == make_state(4, 7, 0)
+    assert r == 5.0
+    assert d is True
+
+
+def test_penalize_invalid_actions():
+    e = TradingEnvironmentTableWrapper(1000.0, "test_model_data.csv", 10)
+    _, r, _, _ = e.step(1)
+    assert r == 0.0
+    _, r, _, _ = e.step(1)
+    assert r == -10.0
+    _, r, _, _ = e.step(2)
+    assert r == 1.0
+    _, r, _, _ = e.step(2)
+    assert r == -10.0
