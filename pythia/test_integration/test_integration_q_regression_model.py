3526fbce9f98c7f240070578f8f7222a03205242
Bernhard <b.raml@gmx.at>
2018-02-12 21:24:38 +0100

    - refactored q regression model integration test to use pytest

--- pythia/test_integration/test_integration_q_regression_model.py
+++ pythia/test_integration/test_integration_q_regression_model.py
@@ -1,48 +1,54 @@
-import unittest
-
 import numpy as np
+import pytest
 
 from pythia.reinforcement.q_model import InvalidDataSize
 from pythia.reinforcement.q_regression_model import QRegressionModel
 
 
-class QRegressionModelTests(unittest.TestCase):
-    def test_wrong_state_size(self):
-        model = self.make_model(2)
-        with self.assertRaises(InvalidDataSize) as cm1:
-            model.predict(np.array([[1, 2, 3]]))
-        self.assertEqual("Input size required is 2, but received state has length 3", cm1.exception.args[0])
-        with self.assertRaises(InvalidDataSize) as cm2:
-            model.train(np.array([[1, 2, 3]]), np.array([[1]]))
-        self.assertEqual("Input size required is 2, but received state has length 3", cm2.exception.args[0])
-
-    @staticmethod
-    def make_model(input_size, hidden=None):
-        if hidden is None:
-            hidden = [1]
-        return QRegressionModel(input_size, hidden)
-
-    def test_wrong_action_size(self):
-        model = self.make_model(2)
-        with self.assertRaises(InvalidDataSize) as cm:
-            model.train(np.array([[1, 2]]), np.array([[1, 2]]))
-        self.assertEqual("Output size of the model is 1, the training model_data has length 2", cm.exception.args[0])
-
-    def test_states_and_targets_not_matching(self):
-        model = self.make_model(2)
-        with self.assertRaises(InvalidDataSize) as cm:
-            model.train(np.array([[1, 2]]), np.array([[1], [2]]))
-        self.assertEqual("The number of states (1) differs from the numbers of targets (2)", cm.exception.args[0])
-
-    def test_initialization(self):
-        model = self.make_model(1, [1])
-        self.assertIsNotNone(model.predict(np.array([[1]]))[0])
-
-    def test_train(self):
-        model = self.make_model(1, [1])
-        model.train(np.array([[1]]), np.array([[1]]))
-        self.assertNotAlmostEqual(0, model.predict(np.array([[1]]))[0])
+def make_model(input_size, hidden=None):
+    if hidden is None:
+        hidden = [1]
+    return QRegressionModel(input_size, hidden)
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def two_in_model():
+    return make_model(2)
+
+
+@pytest.fixture
+def deep_model():
+    return make_model(1, [1])
+
+
+def test_predicting_with_wrong_state_size(two_in_model):
+    with pytest.raises(InvalidDataSize) as e_info:
+        two_in_model.predict(np.array([[1, 2, 3]]))
+    assert str(e_info.value) == "Input size required is 2, but received state has length 3"
+
+
+def test_training_with_wrong_state_size(two_in_model):
+    with pytest.raises(InvalidDataSize) as e_info:
+        two_in_model.train(np.array([[1, 2, 3]]), np.array([[1]]))
+    assert str(e_info.value) == "Input size required is 2, but received state has length 3"
+
+
+def test_wrong_action_size(two_in_model):
+    with pytest.raises(InvalidDataSize) as e_info:
+        two_in_model.train(np.array([[1, 2]]), np.array([[1, 2]]))
+    assert str(e_info.value) == "Output size of the model is 1, the training model_data has length 2"
+
+
+def test_states_and_targets_not_matching(two_in_model):
+    with pytest.raises(InvalidDataSize) as e_info:
+        two_in_model.train(np.array([[1, 2]]), np.array([[1], [2]]))
+    assert str(e_info.value) == "The number of states (1) differs from the numbers of targets (2)"
+
+
+def test_initialization(deep_model):
+    assert deep_model.predict(np.array([[1]]))[0] is not None
+
+
+def test_train(deep_model):
+    deep_model.train(np.array([[1]]), np.array([[1]]))
+    assert deep_model.predict(np.array([[1]]))[0] != 0
