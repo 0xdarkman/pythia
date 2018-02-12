692f5fe5c3f15e80d16c286765b1e8a3d54e68c0
Bernhard <b.raml@gmx.at>
2018-02-12 21:27:34 +0100

    - refactored stock data integration test to use pytest

--- pythia/test_integration/test_stock_data.py
+++ pythia/test_integration/test_stock_data.py
@@ -1,32 +1,22 @@
-import unittest
-
 import numpy as np
+import pytest
 
 from pythia.environment.stocks import StockData, ExceededStockData
 
 
-def make_stock_data():
+@pytest.fixture
+def stocks():
     return StockData("../test_integration/test_stock_data.csv")
 
 
-class StockDataTests(unittest.TestCase):
-    def test_get_period_returns_sequence_of_stock_data(self):
-        stocks = make_stock_data()
-
-        period = stocks.get_period(1, 3)
-
-        self.assertTrue(
-            np.array_equal(np.array([[4.25, 4.31, 3.90, 4.09, 272321],
-                                     [4.17, 4.21, 4.05, 4.21, 126586],
-                                     [4.26, 4.30, 4.11, 4.18, 70590]]),
-                           period))
-
-    def test_period_exceeds_data_throws_exception(self):
-        stocks = make_stock_data()
-
-        with self.assertRaises(ExceededStockData):
-            stocks.get_period(20, 10)
+def test_get_period_returns_sequence_of_stock_data(stocks):
+    period = stocks.get_period(1, 3)
+    assert np.array_equal(np.array([[4.25, 4.31, 3.90, 4.09, 272321],
+                                    [4.17, 4.21, 4.05, 4.21, 126586],
+                                    [4.26, 4.30, 4.11, 4.18, 70590]]),
+                          period)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_period_exceeds_data_throws_exception(stocks):
+    with pytest.raises(ExceededStockData):
+        stocks.get_period(20, 10)
