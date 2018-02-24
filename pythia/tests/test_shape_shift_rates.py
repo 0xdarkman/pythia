bb0cb3e7a4c3e482abb1f1dd9fa213c1251ab1bd
Bernhard <b.raml@gmx.at>
2018-02-24 15:24:33 +0100

    PYTHIA-9 Implemented data wrapper for shapeshift.io exchange rate data

---
+++ pythia/tests/test_shape_shift_rates.py
@@ -0,0 +1,80 @@
+import io
+
+import pytest
+
+from pythia.streams.shape_shift_rates import RatesPair, ShapeShiftRates
+
+
+class RecordsStub(io.StringIO):
+    def add_record(self, *pairs):
+        string = "{2018-02-23 07:55:01.668919: b'[" + ",".join(map(str, pairs)) + "]'}\n"
+        self.write(string)
+        return self
+
+    def finish(self):
+        self.seek(0)
+
+
+class PairEntryStub(RatesPair):
+    def __init__(self, pair, rate, limit, maxLimit, min, minerFee):
+        super().__init__({"pair": pair,
+                          "rate": rate,
+                          "limit": limit,
+                          "maxLimit": maxLimit,
+                          "min": min,
+                          "minerFee": minerFee})
+
+    def __eq__(self, other):
+        return self.rate == other.rate and \
+               self.limit == other.limit and \
+               self.maxLimit == other.maxLimit and \
+               self.min == other.min and \
+               self.minerFee == other.minerFee
+
+
+@pytest.fixture
+def empty():
+    stream = io.StringIO("")
+    yield ShapeShiftRates(stream)
+    stream.close()
+
+
+@pytest.fixture
+def stream():
+    stream = RecordsStub()
+    yield stream
+    stream.close()
+
+
+def test_empty(empty):
+    with pytest.raises(StopIteration) as e:
+        unused = next(iter(empty))
+
+
+def test_one_pair_entry(stream):
+    pair = PairEntryStub("ETH_SALT", 1.1, 0.7, 6.2, 0.1, 0.5)
+    stream.add_record(pair).finish()
+    single = ShapeShiftRates(stream)
+    pairs = next(iter(single))
+    assert pairs["ETH_SALT"] == pair
+
+
+def test_multiple_pair_entries(stream):
+    pairA = PairEntryStub("ETH_SALT", 1.1, 0.7, 6.2, 0.1, 0.5)
+    pairB = PairEntryStub("RCN_1ST", 1.52, 0.1, 4.1, 1.1, 1.9)
+    stream.add_record(pairA, pairB).finish()
+    single = ShapeShiftRates(stream)
+    pairs = next(iter(single))
+    assert pairs["ETH_SALT"] == pairA
+    assert pairs["RCN_1ST"] == pairB
+
+
+def test_multiple_records(stream):
+    pair1 = PairEntryStub("ETH_SALT", 1.1, 0.7, 6.2, 0.1, 0.5)
+    pair2 = PairEntryStub("ETH_SALT", 1.2, 0.5, 6.7, 0.3, 0.9)
+    stream.add_record(pair1).add_record(pair2).finish()
+    multi = ShapeShiftRates(stream)
+    pairs1 = next(iter(multi))
+    pairs2 = next(iter(multi))
+    assert pairs1["ETH_SALT"] == pair1
+    assert pairs2["ETH_SALT"] == pair2
