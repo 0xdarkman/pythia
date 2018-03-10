bdb30bff3814d0b709d7ac900015858840baa97b
Bernhard <b.raml@gmx.at>
2018-03-10 10:59:20 +0100

    [PYTHIA-18] Implement crypto wallet environment for reinforcement AI

---
+++ pythia/core/environment/crypto_ai_environment.py
@@ -0,0 +1,103 @@
+from decimal import Decimal
+from collections import deque
+from functools import reduce
+
+from pythia.core.environment.crypto_environment import CryptoEnvironment, EnvironmentFinished
+
+
+class ExchangeRanges:
+    def __init__(self):
+        self.ranges = dict()
+
+    def extend(self, pair):
+        if pair.name not in self.ranges:
+            self.ranges[pair.name] = pair
+        else:
+            self.ranges[pair.name].extend(pair)
+
+        return self
+
+    def normalize_rate(self, name, rate):
+        r = self.ranges[name]
+        return (float(rate) - r.min) / (r.max - r.min)
+
+    def __len__(self):
+        return len(self.ranges)
+
+
+class PairRanges:
+    def __init__(self, named_pair):
+        name, pair = named_pair
+        self.name = name
+        self.min = float(pair.rate)
+        self.max = float(pair.rate)
+
+    def extend(self, other):
+        assert self.name == other.name
+        self.min = min(self.min, other.min)
+        self.max = max(self.max, other.max)
+        return self
+
+
+def _pairs_to_extremes(pairs):
+    return map(lambda pair: PairRanges(pair), pairs.items())
+
+
+def _calculate_exchange_ranges(rates):
+    def extend_exchange_ranges(ranges, pairs):
+        return reduce(lambda r, p: r.extend(p), _pairs_to_extremes(pairs), ranges)
+
+    return reduce(extend_exchange_ranges, rates, ExchangeRanges())
+
+
+class CryptoAiEnvironment(CryptoEnvironment):
+    def __init__(self, rates, start_coin, start_amount, window_size, index_to_coin, reward_calc, exchange_filter=None):
+        self.exchange_ranges = _calculate_exchange_ranges(rates)
+        self.index_to_coin = index_to_coin
+        self.coin_to_index = {v: k for k, v in self.index_to_coin.items()}
+        self.reward_calc = reward_calc
+        self.exchange_filter = exchange_filter
+        self.starting_balance = Decimal(start_amount)
+        num_exchanges = len(self.exchange_ranges) if exchange_filter is None else len(exchange_filter)
+        self.window = deque([], maxlen=(window_size * num_exchanges))
+        self.state = None
+        rates.reset()
+        super().__init__(rates, start_coin, start_amount)
+
+    def reset(self):
+        self.window.clear()
+        self._append_normalized_pairs(super().reset()[1])
+        self._fill_window()
+        return self._next_ai_state()
+
+    def _next_ai_state(self):
+        b = self.balance_in(self.index_to_coin[0])
+        nb = (b - self.starting_balance) / self.starting_balance
+        self.state = [self.coin_to_index[self.coin], float(nb)] + list(self.window)
+        return self.state
+
+    def _fill_window(self):
+        while not len(self.window) == self.window.maxlen:
+            try:
+                s, _, _, _ = super().step(None)
+                self._append_normalized_pairs(s[1])
+            except EnvironmentFinished:
+                raise WindowError("There is not enough data to fill the window of size {}".format(self.window.maxlen))
+
+    def _append_normalized_pairs(self, pairs):
+        def filter_rates(t):
+            if self.exchange_filter is None:
+                return True
+            return t[0] in self.exchange_filter
+
+        for n, pair in filter(filter_rates, pairs.items()):
+            self.window.append(self.exchange_ranges.normalize_rate(n, pair.rate))
+
+    def step(self, action):
+        s, _, done, _ = super().step(self.index_to_coin[action] if action is not None else None)
+        self._append_normalized_pairs(s[1])
+        return self._next_ai_state(), self.reward_calc(self), done, _
+
+
+class WindowError(Exception):
+    pass
---
+++ pythia/tests/test_crypto_ai_environment.py
@@ -0,0 +1,127 @@
+import pytest
+
+from pythia.core.environment.crypto_ai_environment import CryptoAiEnvironment, WindowError
+from pythia.tests.doubles import RecordsStub, RatesStub, entry
+
+
+class AiRatesStub(RatesStub):
+    def add_pairs(self, *pairs):
+        for line in zip(*pairs):
+            self.add_record(*(entry(str(i), str(rate)) for i, rate in enumerate(line)))
+        self.finish()
+        return self
+
+
+class RewardCalculatorStub:
+    def __init__(self, reward):
+        self.reward = reward
+
+    def __call__(self, env):
+        return self.reward
+
+
+class RewardCalculatorSpy:
+    def __init__(self):
+        self.received_arg = None
+
+    def __call__(self, env):
+        self.received_arg = env
+
+
+@pytest.fixture
+def rates():
+    s = RecordsStub()
+    yield AiRatesStub(s)
+    s.close()
+
+
+@pytest.fixture
+def calc_spy():
+    return RewardCalculatorSpy()
+
+def make_env(rates, start_coin="0", start_amount="1", window=1, exchange_filter=None, index_to_coin=None,
+             reward_calc=RewardCalculatorStub(0)):
+    index_to_coin = {0: '0', 1: '1'} if index_to_coin is None else index_to_coin
+    return CryptoAiEnvironment(rates, start_coin, start_amount, window, index_to_coin, reward_calc, exchange_filter)
+
+
+def test_data_too_small_for_window(rates):
+    rates.add_record(entry("BTC_ETH", "1")).add_record(entry("BTC_ETH", "2")).finish()
+    with pytest.raises(WindowError) as e:
+        make_env(rates, window=3)
+    assert str(e.value) == "There is not enough data to fill the window of size 3"
+
+
+def test_rates_are_normalized(rates):
+    rates.add_record(entry("BTC_ETH", "2")).add_record(entry("BTC_ETH", "3")).add_record(entry("BTC_ETH", "1")).finish()
+    assert make_env(rates, window=3).reset() == [0, 0.0, 0.5, 1.0, 0.0]
+
+
+@pytest.mark.parametrize("size, expected_state", [
+    (1, [0, 0.0, 0.5]),
+    (2, [0, 0.0, 0.5, 1.0]),
+    (3, [0, 0.0, 0.5, 1.0, 0.0])
+])
+def test_window_sizes(rates, size, expected_state):
+    rates.add_record(entry("BTC_ETH", "2")).add_record(entry("BTC_ETH", "3")).add_record(entry("BTC_ETH", "1")).finish()
+    assert make_env(rates, window=size).reset() == expected_state
+
+
+def test_multiple_pairs(rates):
+    rates.add_pairs((2, 1, 3), (10, 12, 20))
+    assert make_env(rates, window=3).reset() == [0, 0.0, 0.5, 0.0, 0.0, 0.2, 1.0, 1.0]
+
+
+@pytest.mark.parametrize("allowed_rate, expected_state", [
+    ('0', [0, 0.0, 0.5, 0.0, 1.0]),
+    ('1', [0, 0.0, 0.0, 0.2, 1.0]),
+])
+def test_filter_pairs(rates, allowed_rate, expected_state):
+    rates.add_pairs((2, 1, 3), (10, 12, 20))
+    assert make_env(rates, window=3, exchange_filter=[allowed_rate]).reset() == expected_state
+
+
+def test_step_moves_window(rates):
+    rates.add_pairs((2, 1, 3), (10, 12, 20))
+    s, _, _, _ = make_env(rates, window=2).step(None)
+    assert s[-4:] == [0.0, 0.2, 1.0, 1.0]
+
+
+def test_last_state_returns_done(rates):
+    rates.add_pairs((2, 1, 3), (10, 12, 20))
+    _, _, done, _ = make_env(rates, window=2).step(None)
+    assert done
+
+
+def test_exchange_coin(rates):
+    rates.add_record(entry("BTC_ETH", "2"), entry("ETH_BTC", "10")) \
+        .add_record(entry("BTC_ETH", "1"), entry("ETH_BTC", "12")) \
+        .add_record(entry("BTC_ETH", "3"), entry("ETH_BTC", "20")).finish()
+    s, _, _, _ = make_env(rates, start_coin="BTC", index_to_coin={0: "BTC", 1: "ETH"}).step(1)
+    assert s[0] == 1
+
+
+def test_normalized_balance(rates):
+    rates.add_record(entry("BTC_ETH", "2", "0"), entry("ETH_BTC", "0.5", "0")) \
+        .add_record(entry("BTC_ETH", "1", "0"), entry("ETH_BTC", "1", "0")).finish()
+    s, _, _, _ = make_env(rates, start_coin="BTC", index_to_coin={0: "BTC", 1: "ETH"}).step(1)
+    assert s[1] == 1.0
+
+
+def test_returns_calculated_reward(rates):
+    rates.add_pairs((1, 2, 3))
+    _, r, _, _ = make_env(rates, reward_calc=RewardCalculatorStub(10.0)).step(None)
+    assert r == 10.0
+
+
+def test_reward_calc_receives_environment(rates, calc_spy):
+    env = make_env(rates.add_pairs((1, 2, 3)), reward_calc=calc_spy)
+    env.step(None)
+    assert calc_spy.received_arg is env
+
+
+def test_environment_has_last_state(rates):
+    env = make_env(rates.add_pairs((1, 2, 3)))
+    env.step(None)
+    assert env.state == [0, 0.0, 0.5]
+
