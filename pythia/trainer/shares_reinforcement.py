15dff0c3ae88b21576a0799a69aecc342f01fdbe
Bernhard <b.raml@gmx.at>
2018-06-09 11:28:03 +0200

    added support for checkpoint saving and restoring in preparation of cloud computing

--- pythia/trainer/shares_reinforcement.py
+++ pythia/trainer/shares_reinforcement.py
@@ -24,28 +24,38 @@ def run_shares_model(holding_tokens,
                      num_steps,
                      gamma,
                      alpha,
-                     episodes):
+                     episodes,
+                     output_dir=None):
     token_h = "CURRENCY" if holding_tokens is None else os.path.basename(holding_tokens)
     token_b = os.path.basename(buying_tokens)
-    with open(buying_tokens) as stream, tf.Session():
+
+    with open(buying_tokens) as stream, tf.Session() as sess:
         with clock_block("Initialization"):
             rates = ShareRates(Symbol(token_b, stream))
             env = RatesAiEnvironment(rates, token_h, starting_balance, window, {1: token_h, 2: token_b},
                                      TotalBalanceReward())
 
-            model = QRegressionModel(3 + window * 2, hidden_layers, learning_rate)
+            model = QRegressionModel(3 + window * 2, hidden_layers, learning_rate, 42)
+            saver, ckpt = None, None
+            if output_dir is not None:
+                saver, ckpt = tf.train.Saver(), os.path.join(output_dir, "model.ckpt")
+            if ckpt is not None and tf.train.checkpoint_exists(ckpt):
+                saver.restore(sess, ckpt)
+
             Q = QNeuronal(model, memory_size)
             episode = 0
             policy = NormalEpsilonGreedyPolicy(lambda: epsilon_episode_start / (episode + 1), ActionFilter(env))
             agent = TDAgent(policy, Q, num_steps, gamma, alpha)
-            sess = RatesExchangeSession(env, agent)
+            rates_sess = RatesExchangeSession(env, agent)
 
         for e in range(episodes):
             episode = e
             with clock_block("Running"):
-                sess.run()
+                rates_sess.run()
             print("Episode {} finished.".format(episode))
-            print("The td agent crated a token difference of: {0}".format(sess.difference()))
+            print("The td agent crated a token difference of: {0}".format(rates_sess.difference()))
+            if output_dir is not None:
+                saver.save(sess, ckpt)
 
         print("Current balance: {0} {1}".format(env.amount, env.token))
 
@@ -63,5 +73,6 @@ if __name__ == '__main__':
         gamma=0.9,
         alpha=0.2,
         num_steps=10,
-        episodes=300
+        episodes=300,
+        output_dir="../output"
     )
