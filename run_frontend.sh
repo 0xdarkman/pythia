aea8d7d700b3bf586e2e31737e77e7f92ba8a35a
Bernhard <b.raml@gmx.at>
2019-04-30 19:01:59 +0200

    Update frontend runner script
    To allow running as a systemd service the pythonpath is now determined
    by the location of the runner bash script itself.

--- run_frontend.sh
+++ run_frontend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=pwd
-gunicorn --certfile $1 --keyfile $2 -b 0.0.0.0:4000 wsgi:app
+export PYTHONPATH="$( dirname "${BASH_SOURCE[0]}" )"
+${PYTHONPATH}/venv/bin/gunicorn --certfile $1 --keyfile $2 -b 0.0.0.0:4000 wsgi:app
